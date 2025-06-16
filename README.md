bb7260abcb10081e40730d6d9cc556422227132c
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-16 10:50:55 +0530

    Added basic signup card for auth page

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React + Vite
 
-ShadCN Setup: ![Link](https://ui.shadcn.com/docs/installation/vite)Add commentMore actions
+Shadcn Setup: ![Link](https://ui.shadcn.com/docs/installation/vite)Add commentMore actions
 
 Atom Design : ![Link](https://medium.com/@janelle.wg/atomic-design-pattern-how-to-structure-your-react-application-2bb4d9ca5f97)
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
-import { Button } from "@/components/ui/button"
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Auth } from './pages/Auth/Auth';
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
   )
 }
 
