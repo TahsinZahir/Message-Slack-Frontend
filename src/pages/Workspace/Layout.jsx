5cc223722ace7ed261fe293d3029bc9ef4774225
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-22 01:21:13 +0530

    Added more workspace ui

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,9 +1,12 @@
 import { WorkspaceSidebar } from '@/components/organisms/Workspace/WorkspaceSidebar';
+import { WorkspaceNavbar } from '@/components/organisms/Workspace/WorkspaceNavbar';
 
 export const WorkspaceLayout = ({ children }) => {
     return (
         <div className="h-[100vh] ">
             <div className="flex h-full ">
+                <WorkspaceNavbar />
+            <div className="flex h-[calc(100vh-40px)]"></div>
                 <WorkspaceSidebar />
                 {children}
             </div>
