9c040f4257c27946594d0e8f2b3340d6d680f7b9
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-23 01:38:03 +0530

    Added resizable sidebar for building channel list

--- src/pages/Workspace/Layout.jsx
+++ src/pages/Workspace/Layout.jsx
@@ -1,6 +1,6 @@
 import { WorkspaceSidebar } from '@/components/organisms/Workspace/WorkspaceSidebar';
 import { WorkspaceNavbar } from '@/components/organisms/Workspace/WorkspaceNavbar';
-
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
 export const WorkspaceLayout = ({ children }) => {
     return (
         <div className="h-[100vh] flex">
@@ -8,7 +8,24 @@ export const WorkspaceLayout = ({ children }) => {
             <div className="flex flex-col flex-1">
                 <WorkspaceNavbar />
             <div className="flex-1 overflow-y-auto bg-white"></div>
-                {children}
+               <ResizablePanelGroup direction="horizontal" autoSaveId={'workspace-resize'}>
+                    <ResizablePanel
+                        defaultSize={20}
+                        minSize={11}
+                        className="bg-purple-200"
+
+                    >
+                        <div>
+                            Sidebar
+                        </div>
+                    </ResizablePanel>
+                    <ResizableHandle withHandle/>
+                    <ResizablePanel
+                        minSize={20}
+                    >
+                        {children}
+                    </ResizablePanel>
+                </ResizablePanelGroup>
             </div>
         </div>
     );
