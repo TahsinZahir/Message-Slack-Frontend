7b7d9d679e0906d96684ebc3f3de5181cd3ff585
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-26 01:28:41 +0530

    Added panel header

--- src/components/molecules/CreateChannelModal/CreateChannelModal.jsx
+++ src/components/molecules/CreateChannelModal/CreateChannelModal.jsx
@@ -1,22 +1,41 @@
 import { useState } from 'react';
-
+import { useQueryClient } from '@tanstack/react-query';
 import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
 import { useCreateChannelModal } from '@/hooks/context/useCreateChannelModal';
+import { useAddChannelToWorkspace } from '@/hooks/apis/workspaces/useAddChannelToWorkspace';
+import { useCurrentWorkspace } from '@/hooks/context/useCurrentWorkspace';
+import { toast } from "sonner"
 
 export const CreateChannelModal = () => {
-    
+      const queryClient = useQueryClient();
     const { openCreateChannelModal, setOpenCreateChannelModal } = useCreateChannelModal();
 
     const [channelName, setChannelName] = useState('');
+
+    const { addChannelToWorkspaceMutation } = useAddChannelToWorkspace();
+       const {currentWorkspace} = useCurrentWorkspace();
 
     function handleClose() {
         setOpenCreateChannelModal(false);
     }
 
-    function handleFormSubmit(e) {
+    async function handleFormSubmit(e) {
         e.preventDefault();
+        await addChannelToWorkspaceMutation({
+            workspaceId: currentWorkspace?._id,
+            channelName: channelName
+        });
+
+        toast({
+            type: 'success',
+            title: 'Channel created successfully'
+        });
+
+        queryClient.invalidateQueries(`fetchWorkspaceById-${currentWorkspace?._id}`);
+
+        handleClose();
     }
 
     return (
--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -4,9 +4,11 @@ import { AuthContextProvider } from './AuthContext';
 import { CreateChannelContextProvider } from './CreateChannelContext';
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
 import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
+import { WorkspaceContextProvider } from './WorkspaceContext';
 
 export const AppContextProvider = combineContext(
     AuthContextProvider,
+    WorkspaceContextProvider,
     CreateWorkspaceContextProvider,
      WorkspacePreferencesModalContextProvider,
       CreateChannelContextProvider
