df7cc02b4312d6eaba3a762596f1fcd6c4f35c98
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-28 00:56:50 +0530

    Added logout logic

--- src/components/organisms/Workspace/WorkspaceNavbar.jsx
+++ src/components/organisms/Workspace/WorkspaceNavbar.jsx
@@ -1,8 +1,9 @@
 import { InfoIcon, LucideLoader2, SearchIcon } from 'lucide-react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Button } from '@/components/ui/button';
 import { useGetWorkspaceById } from '@/hooks/apis/workspaces/useGetWorkspaceById';
+import { useAuth } from '@/hooks/context/useAuth';
 import { useCurrentWorkspace } from '@/hooks/context/useCurrentWorkspace';
 
 
@@ -10,13 +11,23 @@ export const WorkspaceNavbar = () => {
 
     const { workspaceId } = useParams();
 
-    const { isFetching, workspace } = useGetWorkspaceById(workspaceId);
+  const navigate = useNavigate();
+    const { logout } = useAuth();
+    const { isFetching, workspace, error, isSuccess } = useGetWorkspaceById(workspaceId);
      const { setCurrentWorkspace } = useCurrentWorkspace();
      useEffect(() => {
+        if(!isFetching && !isSuccess && error) {
+            console.log('Error fetching workspace', error.status);
+            if(error.status === 403) {
+                logout();
+                navigate('/auth/signin');
+            }
+        }
+
         if(workspace) {
             setCurrentWorkspace(workspace);
         }
-    }, [workspace, setCurrentWorkspace]);
+    }, [workspace, setCurrentWorkspace, isSuccess, error, isFetching]);
     
     if(isFetching) {
         return <LucideLoader2 className="animate-spin ml-2" />;
