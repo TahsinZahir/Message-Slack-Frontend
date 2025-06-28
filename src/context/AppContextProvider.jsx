6176e7d987c4d1a536b305bb04dab30a4a61c19e
TahsinZahir <tahsindbg0142@gmail.com>
2025-06-29 02:28:06 +0530

    Added socket.io client

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -3,10 +3,12 @@ import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
 import { CreateChannelContextProvider } from './CreateChannelContext';
 import { CreateWorkspaceContextProvider } from './CreateWorkspaceContext';
+import { SocketContextProvider } from './SocketContext';
 import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
 import { WorkspaceContextProvider } from './WorkspaceContext';
 
 export const AppContextProvider = combineContext(
+     SocketContextProvider,
     AuthContextProvider,
     WorkspaceContextProvider,
     CreateWorkspaceContextProvider,
