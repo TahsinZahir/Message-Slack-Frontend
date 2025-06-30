afda247858e58b08f69cb840dff0afd3be06a635
TahsinZahir <tahsindbg0142@gmail.com>
2025-07-01 00:02:49 +0530

    Fix typo

--- src/hooks/context/useChannelMessages.js
+++ src/hooks/context/useChannelMessages.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import ChannelMessages from '@/context/CHannelMessages';
+import ChannelMessages from '@/context/ChannelMessages';
 
 export const useChannelMessages = () => {
     return useContext(ChannelMessages);
